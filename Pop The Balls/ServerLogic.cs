25b830f1e01d1983f9169d6718f6bd5236ca1970
LaurentQUERSIN <quersin.laurent@gmail.com>
2015-09-10 09:45:45 +0200

    fix ball despawn

--- Pop The Balls/ServerLogic.cs
+++ Pop The Balls/ServerLogic.cs
@@ -184,7 +184,7 @@ namespace Pop_The_Balls
                         Ball temp;
                         if (ball.creationTime + 15000 > _env.Clock)
                         {
-                            //_scene.Broadcast("destroy_ball", s => { var writer = new BinaryWriter(s, Encoding.UTF8, false); writer.Write(ball.id); }, PacketPriority.MEDIUM_PRIORITY, PacketReliability.RELIABLE_SEQUENCED);
+                            _scene.Broadcast("destroy_ball", s => { var writer = new BinaryWriter(s, Encoding.UTF8, false); writer.Write(ball.id); }, PacketPriority.MEDIUM_PRIORITY, PacketReliability.RELIABLE);
                             _balls.TryRemove(ball.id, out temp);
                         }
                     }
