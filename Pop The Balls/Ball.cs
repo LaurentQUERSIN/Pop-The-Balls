d99b35852618bc12aa2ee887ad68fc72c99a9708
LaurentQUERSIN <quersin.laurent@gmail.com>
2015-09-10 10:30:07 +0200

    fix click detection

--- Pop The Balls/Ball.cs
+++ Pop The Balls/Ball.cs
@@ -21,11 +21,13 @@ namespace Pop_The_Balls
 
         public bool IsClicked(float player_x, float player_y, long time, ISceneHost scene)
         {
+            float dist = 1.0f;
+
             float updated_x = x + (vx * (time - creationTime) / 1000);
             float updated_y = y + (vy * (time - creationTime) / 1000);
 
             scene.GetComponent<ILogger>().Debug("main", "ball_c : " + updated_x.ToString() + " " + updated_y.ToString() + " || player : " + player_x.ToString() + " " + player_y.ToString());
-            if (updated_x - 0.3f < player_x && player_x < updated_x + 0.3f && updated_y - 0.3f < player_y && player_y < updated_y + 0.3f)
+            if (updated_x - dist < player_x && player_x < updated_x + dist && updated_y - dist < player_y && player_y < updated_y + dist)
                 return (true);
             return (false);
         }
